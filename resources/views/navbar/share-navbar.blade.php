4130830ea27ceab0f954575e4381feff6c558afe
utbuiquoc <utbuiquoc@gmail.com>
2021-10-10 09:58:40 +0700

    edit share-navbar

--- resources/views/navbar/share-navbar.blade.php
+++ resources/views/navbar/share-navbar.blade.php
@@ -8,10 +8,24 @@
 @endsection
 
 @section('right')
-    <div class="user__info">
-        <h6 class="user__name">Khách</h6>
-        <p class="user__id">#0</p>
-    </div>
+    <?php
+        if (Auth::user() !== null) {
+            ?>
+            <div class="user__info">
+                <h6 class="user__name">{{Auth::user()->name}}</h6>
+                <p class="user__id">#{{Auth::user()->id}}</p>
+            </div>
+            <?php
+        } else {
+            ?>
+            <div class="user__info">
+                <h6 class="user__name">Khách</h6>
+                <p class="user__id">#0</p>
+            </div>
+            <?php
+        }
+    ?>
+    
     <div class="user__avatar">
         <img src="img/avatar/default/default-avatar.png" alt="default-avatar" class="user__img">
     </div>
